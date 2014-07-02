5cea07c65220e63f053a9c90228cbe35bd1aca49
Spork Schivago <SporkSchivago@gmail.com>
2014-07-02 19:17:48 -0400

    Added Gzorin's RSXGL library for the PS3 to the PS3Libraries.

---
+++ scripts/026-RSXGL.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -e
+# RSXGL.sh by Spork Schivago (SporkSchivago@gmail.com)
+RSXGL=RSXGL
+
+## Remove the directory and download the source code.
+rm -rf ${RSXGL}
+git clone https://github.com/gzorin/${RSXGL}
+
+## Change to the source code.
+cd ${RSXGL} && mkdir build-ppu
+
+## Patch the source if a patch exists.
+if [ -f ../../patches/${RSXGL}.patch ]; then
+  echo "patching ${RSXGL}..."
+  cat ../../patches/${RSXGL}.patch | patch -p1;
+fi
+
+## Run Autogen
+echo -ne "Running autogen on ${RSXGL}, please wait : "
+NOCONFIGURE=1 ./autogen.sh >& ./autogen_${RSXGL}.log  || \
+	(echo "Error!" && \
+	(tail ./autogen_${RSXGL}.log || true) && \
+	echo -ne "\n\nSee autogen_${RSXGL}.log for details.\n" && \
+	exit 1)
+
+## Enter the build directory
+cd build-ppu
+
+## Configure the build.
+../configure
+
+## Compile and install.
+${MAKE:-make} && ${MAKE:-make} install
