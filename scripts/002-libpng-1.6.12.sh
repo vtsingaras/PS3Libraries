c4da0bb5fed41600212072c5e897ee1d6381aa15
Spork Schivago <SporkSchivago@gmail.com>
2014-07-04 13:39:22 -0400

    Updated libpng-1.6.6 to libpng-1.6.12.  This should fix some of the bugs that where found in the previous versions of libpng

--- scripts/002-libpng-1.6.12.sh
+++ scripts/002-libpng-1.6.12.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -e
-# libpng-1.6.6.sh by Dan Peori (danpeori@oopo.net) (updated by Spork Schivago)
+# libpng-1.6.12.sh by Dan Peori (danpeori@oopo.net) (updated by Spork Schivago)
 
-LIBPNG="libpng-1.6.6"
+LIBPNG_VER="1.6.12"
+LIBPNG="libpng-${LIBPNG_VER}"
 ZLIBLIB="${PS3DEV}/portlibs/ppu/lib" export ZLIBLIB
 ZLIBINC="${PS3DEV}/portlibs/ppu/include" export ZLIBINC
 CPPFLAGS="-I${ZLIBINC} -I${PS3DEV}/portlibs/ppu/include" export CPPFLAGS
@@ -11,10 +12,10 @@ LD_LIBRARY_PATH="${ZLIBLIB}:$LD_LIBRARY_PATH" export LD_LIBRARY_PATH
 PKG_CONFIG_PATH="${PS3DEV}/portlibs/ppu" export PKG_CONFIG_PATH
 
 ## Download the source code.
-wget --continue http://download.sourceforge.net/libpng/${LIBPNG}.tar.gz;
+wget --continue --max-redirect=100 "http://sourceforge.net/projects/libpng/files/libpng16/${LIBPNG_VER}/${LIBPNG}.tar.xz" || wget --continue --max-redirect=100 "http://sourceforge.net/projects/libpng/files/libpng16/older-releases/${LIBPNG_VER}/${LIBPNG}.tar.xz"
 
 ## Unpack the source code.
-rm -Rf ${LIBPNG} && tar -xvzf ${LIBPNG}.tar.gz && cd ${LIBPNG}
+rm -Rf ${LIBPNG} && tar -xvJf ${LIBPNG}.tar.xz && cd ${LIBPNG}
 
 ## Patch the source if a patch exists.
 if [ -f ../../patches/${LIBPNG}-PPU.patch ]; then
@@ -26,7 +27,9 @@ fi
 mkdir build-ppu && cd build-ppu
 
 ## Configure the build.
-../configure --prefix="${PS3DEV}/portlibs/ppu" --host="powerpc64-ps3-elf" --enable-static --disable-shared
+../configure --prefix="${PS3DEV}/portlibs/ppu" --host="powerpc64-ps3-elf" \
+	--enable-static --disable-shared --with-pkgconfigdir="${PS3DEV}/portlibs/ppu/lib/pkgconfig" \
+	--with-zlib-prefix="${PS3DEV}/portlibs/ppu"
 
 ## Compile and install.
-${MAKE:-make} -j4 && ${MAKE:-make} install
+# ${MAKE:-make} -j4 && ${MAKE:-make} install
