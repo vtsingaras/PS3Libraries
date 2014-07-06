cff05faac4016e1066f16022b642def8e46a0236
Spork Schivago <SporkSchivago@gmail.com>
2014-07-05 20:22:19 -0400

    Modified the libmikmod script, once again, to use another sourceforge download link.  We should really host it some where besides sourceforge.

--- scripts/011-libmikmod-3.3.3.sh
+++ scripts/011-libmikmod-3.3.3.sh
@@ -5,7 +5,7 @@ LIBMIKMOD_VER="3.3.6"
 LIBMIKMOD="libmikmod-${LIBMIKMOD_VER}"
 
 ## Download the source code.
-wget --continue "http://downloads.sourceforge.net/project/mikmod/libmikmod/${LIBMIKMOD_VER}/${LIBMIKMOD}.tar.gz?r=http%3A%2F%2Fmikmod.sourceforge.net%2F&ts=1403638643&use_mirror=iweb" -O ${LIBMIKMOD}.tar.gz || wget --continue "http://sourceforge.net/projects/mikmod/files/outdated_versions/libmikmod/${LIBMIKMOD_VER}/${LIBMIKMOD}.tar.gz/download" -O ${LIBMIKMOD}.tar.gz
+wget --max-redirect=1000 "http://sourceforge.net/project/downloading.php?group_id=40531&filename=${LIBMIKMOD}.tar.gz" -O ${LIBMIKMOD}.tar.gz || wget --continue "http://sourceforge.net/projects/mikmod/files/outdated_versions/libmikmod/${LIBMIKMOD_VER}/${LIBMIKMOD}.tar.gz/download" -O ${LIBMIKMOD}.tar.gz
 
 ## Unpack the source code.
 rm -Rf ${LIBMIKMOD} && tar -zxvf ${LIBMIKMOD}.tar.gz && cd ${LIBMIKMOD}
@@ -21,11 +21,11 @@ mkdir build-ppu && cd build-ppu
 
 ## Configure the build.
 CPPFLAGS="-I${PSL1GHT}/ppu/include -I${PS3DEV}/portlibs/ppu/include" \
-LDFLAGS="-L${PSL1GHT}/ppu/lib -L${PS3DEV}/portlibs/ppu/lib" \
+LDFLAGS="-L${PSL1GHT}/ppu/lib -L${PS3DEV}/portlibs/ppu/lib -lrt -llv2" \
 CC="powerpc64-ps3-elf-gcc" LD="powerpc64-ps3-elf-ld" NM="powerpc64-ps3-elf-nm" \
 RANLIB="powerpc64-ps3-elf-ranlib" STRIP="powerpc64-ps3-elf-strip" \
 ../configure --prefix="${PS3DEV}/portlibs/ppu" --host="powerpc64-ps3-elf" \
---disable-esd --disable-dl --disable-shared
+--disable-esd --disable-dl --disable-shared --disable-sdl2
 
 ## Compile and install.
 ${MAKE:-make} && ${MAKE:-make} install
